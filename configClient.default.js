19691b1a7a7bf1e73abe8f164375886a16458824
Vodur <dr.vodur@gmail.com>
2020-06-06 05:28:06 +0000

    Use webpack cache busting for js bundle.

--- configClient.default.js
+++
@@ -1,8 +0,0 @@
-var configClient = {};
-
-/* set to null if using serveStatic in configServer */
-configClient.socketIoHost = null;
-configClient.socketIoPort = null;
-
-configClient.apiHost = null;
-configClient.apiPort = null;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './ts/build/build_client/client/client.js',
   output: {
-    path: path.resolve(__dirname + "/static/public/"),
-    filename: 'mudslinger.js'
+    path: path.resolve(__dirname, "static/public"),
+    filename: 'mudslinger-[contentHash].js'
   },
+  plugins: [new HtmlWebpackPlugin({
+      template: "./template.html",
+      filename: path.resolve(__dirname, "static/public", "index.html")
+  })],
   mode: 'production'
 };
