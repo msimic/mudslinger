b3869b4ae5df06a12c344d66a522e0e8e62f9a39
Vodur <dr.vodur@gmail.com>
2020-06-09 07:36:08 +0000

    Move ioevent.ts

--- telnet_proxy/src/app.ts
+++ telnet_proxy/src/app.ts
@@ -5,7 +5,7 @@ import * as readline from "readline";
 import axios from "axios";
 import * as express from "express";
 
-import { IoEvent } from "../../ts/src/shared/ioevent";
+import { IoEvent } from "../../common/src/ts/ioevent";
 
 let serverConfig = require("../../../configServer.js");
 console.log(serverConfig);
